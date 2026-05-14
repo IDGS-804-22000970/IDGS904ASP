6f65c08e71b42169c47654ea6c1322944a4163da
T <papilozanowest@gmail.com>
2026-05-13 23:53:28 -0600

    Display 2026-05-13

---
+++ IDGS904ASP/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using IDGS904ASP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IDGS904ASP.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public RedirectToRouteResult Index()
+        {
+            return RedirectToAction("Index", "Pruebas");
+        }
+            
+        //public RedirectResult Index()
+        //{
+        //    return Redirect("https://google.com.mx");
+        //}
+
+
+        //public ActionResult Index()
+        //{
+        //    var alumno1 = new Persona() { Nombre = "Juan", Edad = 32, Empleado = true };
+        //    var alumno2 = new Persona() { Nombre = "Mario", Edad = 90, Empleado = false };
+        //    // return Json(alumno1, JsonRequestBehavior.AllowGet);
+        //    // return View();
+        //    return Json(new List<Persona>() { alumno1, alumno2 },JsonRequestBehavior.AllowGet);
+        //}
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
